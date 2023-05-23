94c6995a9744fd1b15f58287592b8afcc7e7a878
Aman Kumar <34418398+Amankumarbbs@users.noreply.github.com>
2023-05-23 13:39:53 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://main--lighthearted-tiramisu-68a1a9.netlify.app/";
 
 
 const GlobalContext = React.createContext()
